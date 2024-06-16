696526c0768f1a643720a516879fb988b5c8c62c
isuruij <2020uelog@gmail.com>
2024-06-16 12:05:04 +0530

    chore: Update server log message for server running status

--- index.js
+++ index.js
@@ -38,5 +38,5 @@ app.get('/test', (req, res) => {
 });
 
 server.listen(3001,host, () => {
-  console.log("SERVER RUNNING ON PORT 3001");
+  console.log("SERVER RUNNING ON PORT 3001 mmmmmmmmm");
 });
